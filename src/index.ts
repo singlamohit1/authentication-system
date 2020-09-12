d2f4abb2bd44a827a95b97f7825bb69951c0999b
Mohit Singla <singlamohit98@gmail.com>
2020-09-12 10:12:26 +0530

    added register and login pages

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { message: 'Hello World!' });
+    res.render('register.ejs', { message: 'Hello World!' });
+});
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
 });
 
 app.listen(PORT, () => {
